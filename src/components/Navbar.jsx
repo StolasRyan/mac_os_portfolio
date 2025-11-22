5109a045a893797f54bd64f6422bba638a82bcb6
StolasRyan <paul.pbronx@gmail.com>
2025-11-22 10:32:54 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import dayjs from "dayjs"
+import { navLinks,navIcons } from "#constants"
+
+
+function Navbar() {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt="logo" />
+            <p className="font-bold">Pavel's Portfilio</p>
+            <ul>
+                {navLinks .map((item)=>(
+                    <li key={item.id}>
+                        <p>{item.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map((icon)=>(
+                    <li key={icon.id}>
+                        <img src={icon.img} className="icon-hover" alt={`icon-${icon.id}`} />
+                    </li>
+                ))}
+            </ul>
+
+            <time datetime="">{dayjs().format("ddd MMM D h:mm A")}</time>
+        </div>
+    </nav>
+    
+  )
+}
+
+export default Navbar
